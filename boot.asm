ba015f9796a48754b9915efa297a20fff6e0c860
Steven Nyeo <limpingstone@gmail.com>
2021-05-17 04:10:47 -0400

    Organize real mode file naming

--- boot.asm
+++ boot.asm
@@ -6,13 +6,13 @@
 [bits 16]
 
 mov bx, boot_msg
-call print_rm
+call rm_print
 
 ; infinite loop to halt the booting screen
 jmp $
 
 ; INCLUDES
-%include "print_rm.asm"
+%include "rm_print.asm"
 
 ; DATA SECTION
 boot_msg:   db `Loading boot sector from Hard Disk...\r\n`, 0
@@ -22,4 +22,5 @@ times 510 - ($ - $$) db 0x00
 
 ; the magic number
 dw 0xaa55
+
 
--- rm_print.asm
+++ rm_print.asm
@@ -1,6 +1,6 @@
 [bits 16]
 
-print_rm:
+rm_print:
     ; reserve ax, bx for use
     push ax
     push bx
@@ -8,10 +8,10 @@ print_rm:
     ; BIOS print mode
     mov ah, 0x0e
 
-print_rm_loop:
+rm_print_loop:
     ; if the character at bx is the end of the string, then end loop
     cmp byte[bx], 0
-    je print_rm_end
+    je rm_print_end
 
     ; otherwise, print the character
     mov al, byte[bx]
@@ -19,9 +19,9 @@ print_rm_loop:
 
     ; move to the next character for next iteration
     inc bx
-    jmp print_rm_loop
+    jmp rm_print_loop
 
-print_rm_end:
+rm_print_end:
     ; restore bx, ax from the stack and resume program
     pop bx
     pop ax
