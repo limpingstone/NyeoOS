7d01ef2e3f44bdf7b563c14b16530dceb6bc7123
Steven Nyeo <limpingstone@gmail.com>
2021-06-20 20:52:15 -0400

    Add clear display function in kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3,5 +3,26 @@
 
 #define STYLE_WB    0x0F
 
+typedef struct __attribute__((packed)) {
+    char character;
+    char style;
+} vga_char;
 
+volatile vga_char *text_area = (vga_char*) VGA_START;
+
+void clear_disp() {
+    vga_char blank_char = {
+        .character = ' ',
+        .style = STYLE_WB
+    };
+
+    for (unsigned int i = 0; i < VGA_EXTENT; i++) {
+        text_area[i] = blank_char;
+    }
+}
+
+int main() {
+    clear_disp();
+    return 0;
+}
 
